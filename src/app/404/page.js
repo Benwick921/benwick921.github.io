ae475e86b2039c25805367e8ac2dae27fd8f798c
Jafor921 <jafor@hotmail.it>
2025-03-07 00:31:47 +0000

    migrated from react-app to nextjs-app

--- src/app/404/page.js
+++
@@ -1,9 +0,0 @@
-export default function Hello() {
-    return (
-      <div>
-        <h1>404</h1>
-      
-      </div>
-    );
-  }
-  
--- src/app/layout.js
+++
@@ -1,8 +0,0 @@
-//wihtout this the website does not works so I need it!
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
-}
